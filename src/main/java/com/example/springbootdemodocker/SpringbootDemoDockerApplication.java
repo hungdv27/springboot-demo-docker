acb63778968f1e5ea9226baeeea2ec6e4d654996
hungdoviet <hungdoviet@lptech.vn>
2023-11-19 16:38:17 +0700

    init

---
+++ src/main/java/com/example/springbootdemodocker/SpringbootDemoDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoDockerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootDemoDockerApplication.class, args);
+  }
+
+}
